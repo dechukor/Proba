e7006ae02e8a83ff368c3513e5dcc0566343c534
Evgeniy.Chukreev <dechukor@gmail.com>
2024-09-16 20:41:17 +0500

    Исправления

--- proba.js
+++ proba.js
@@ -2,10 +2,11 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
-    // slidesPerView: 2,
+    slidesPerView: 'auto',
     spaceBetween: 16,    
     // slidesPerGroup: 1,
-    centeredSlides: true,
+    // centeredSlides: true,
+    centerInsufficientSlides: true,
     
     
     
@@ -18,20 +19,12 @@ const swiper = new Swiper('.swiper', {
     breakpoints: {
       320: {
         // swiper.disable(),
-        slidesPerView: 1,
+        // slidesPerView: 1,
+        
       },
       768: {
-        // swiper.disable(),
-        slidesPerView: 2,
+              
       }
-    },
-  
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  
-    
+    },      
     
   });
